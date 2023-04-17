20e4aa57478b77668f3b22a6effa242e2c4446ad
OulkyUfa1990 <130794561+OulkyUfa1990@users.noreply.github.com>
2023-04-17 12:22:12 +0500

    Add files via upload

---
+++ Wine/BubblyWine.java
@@ -0,0 +1,8 @@
+package Wine;
+
+public class BubblyWine extends Wine{
+    @Override
+    public String getHolidayName(){
+        return "Новый год";
+    }
+}
---
+++ Wine/Wine.java
@@ -0,0 +1,7 @@
+package Wine;
+
+public class Wine extends Drink {
+    public String getHolidayName(){
+        return "День рождения";
+    }
+}
